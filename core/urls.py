4b5adba0d147999f7a0b8e4b414286093107a26a
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2024-08-15 00:57:25 +0300

    first commit

--- core/urls.py
+++ core/urls.py
@@ -1,29 +1,24 @@
-from django.urls import path
-from .views import (
-    ItemDetailView,
-    CheckoutView,
-    HomeView,
-    OrderSummaryView,
-    add_to_cart,
-    remove_from_cart,
-    remove_single_item_from_cart,
-    PaymentView,
-    AddCouponView,
-    RequestRefundView
-)
+"""
+URL configuration for core project.
 
-app_name = 'core'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
-    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
-    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
-         name='remove-single-item-from-cart'),
-    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
-    path('request-refund/', RequestRefundView.as_view(), name='request-refund')
+    path('admin/', admin.site.urls),
+    path('',          include('pages.urls')),
+    path('accounts/', include('accounts.url')),
 ]
